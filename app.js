1212699a8e4997dd8051d928099fc4760d07026d
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-19 12:40:24 -0500

    worked through the five diffrent ways to select a html element with javascript

---
+++ app.js
@@ -0,0 +1,38 @@
+// DOM manipulation
+
+
+
+//GetElementById()
+
+/* const title =document.getElementById('main-heading');
+
+console.log(title); */
+
+
+//GetElementByClassName()
+
+/* const listItem = document.getElementsByClassName('list-items');
+
+console.log(listItem); */
+
+
+// getElementByTagName()
+
+/* const listItem = document.getElementsByTagName('li');
+
+console.log(listItem);
+ */
+
+
+// querySelector()
+
+/* const container = document.querySelector('div');
+
+console.log(container); */
+
+// querySelectorAll()
+
+/* 
+const container = document.querySelectorAll('div');
+
+console.log(container); */
