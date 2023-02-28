e97a7069dd33ff0e22672f4166d85362ac824cb3
salamanderctesiphon <swbrookshire@gmail.com>
2023-02-28 11:56:01 -0600

    completed the assignment

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+
+const paragraph = document.createElement('p');
+content.classList.add('paragraph');
+paragraph.textContent = 'Hey, I\'m red!';
+paragraph.style.color = 'red';
+
+container.appendChild(paragraph);
+
+const heading = document.createElement('h3');
+heading.classList.add('heading');
+heading.textContent = "I'm a blue h3!";
+heading.style.color = 'blue';
+
+container.appendChild(heading);
+
+
+const card = document.createElement('div');
+card.classList.add('card');
+card.style.border = "1px solid black";
+card.style.backgroundColor = "pink";
+
+container.appendChild(card);
+
+const headerOne = document.createElement('h1');
+headerOne.textContent = 'I\'m a div';
+card.appendChild(headerOne);
+
+const para = document.createElement('p');
+para.textContent = "ME TOO!";
+card.appendChild(para);
